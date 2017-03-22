76b9bede8fb0522ad42d663861ed8fa2b4787200
Miroslav Čech <cech@codescale.net>
2017-03-22 19:17:27 +0100

    cleanup

---
+++ src/example/hello_world.py
@@ -0,0 +1,31 @@
+# Hello world example. Doesn't depend on any third party GUI framework.
+# Tested with CEF Python v55.3+.
+
+from cefpython3 import cefpython as cef
+import platform
+import sys
+
+def get_switches():
+   return {
+    "enable-blink-features": 'CSSBoxShadow',
+    #"enable-blink-features": "CSS3TextDecorations,CSSBackDropFilter",
+    }
+
+def main():
+    check_versions()
+    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+    cef.Initialize({}, get_switches())
+    cef.CreateBrowserSync(url="http://localhost/box-shadow.html")
+    cef.MessageLoop()
+    cef.Shutdown()
+
+
+def check_versions():
+    print("[hello_world.py] CEF Python {ver}".format(ver=cef.__version__))
+    print("[hello_world.py] Python {ver} {arch}".format(
+            ver=platform.python_version(), arch=platform.architecture()[0]))
+    assert cef.__version__ >= "55.3", "CEF Python v55.3+ required to run this"
+
+
+if __name__ == '__main__':
+    main()
